b250acefcf9a2161c90982e1196f724684d6c2d6
abbyhulm <abigail.h.hulm@ndsu.edu>
2026-03-16 15:47:00 -0500

    Added Inputs to the HousingLocation Component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -11,15 +11,15 @@ import { HousingLocationInfo } from '../housinglocationinfo';
 })
 export class Home {
     readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
-  housingLocation: HousingLocationInfo = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test city',
-    state: 'ST',
-    photo: `${this.baseUrl}/example-house.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
+housingLocation: HousingLocationInfo = {
+  id: 9999,
+  name: 'Test Home',
+  city: 'Test city',
+  state: 'ST',
+  photo: `${this.baseUrl}/example-house.jpg`,
+  availableUnits: 99,
+  wifi: true,
+  laundry: false,
   };
 }
 
--- src/app/housing-location/housing-location.ts
+++ src/app/housing-location/housing-location.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { HousingLocationInfo } from '../housinglocationinfo';
 
 @Component({
   selector: 'app-housing-location',
-  standalone: true,
   imports: [],
   templateUrl: './housing-location.html',
   styleUrl: './housing-location.css',
 })
-export class HousingLocation { }
+export class HousingLocation { 
+  housingLocation = input.required<HousingLocationInfo>();
+}
